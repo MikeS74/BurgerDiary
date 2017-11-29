1789804d16f2c5c1138169989c0ee2c1951c988e
Michael Sanchez <futurefreq@hotmail.com>
2017-11-29 11:22:20 -0800

    added more db login info

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-//  port: 3306,
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "bbe26416610a5f",
-  password: "898d203a",
-  database: "heroku_ffde5ca58101024"
+  port: 3306,
+  host: "us-cdbr-iron-east-05.cleardb.net" || "localhost",
+  user: "bbe26416610a5f" || "root",
+  password: "898d203a" || "FreshChoice1",
+  database: "heroku_ffde5ca58101024" || "burgers_db"
 //    socketPath: '/var/run/mysqld/mysqld.sock'
 });
 
