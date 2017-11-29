474790d6cb1cb2e889eb4e1fa19ce727e8b73b2c
Michael Sanchez <futurefreq@hotmail.com>
2017-11-29 02:45:48 -0800

    add burger and css working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Chip Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Burger', false);
