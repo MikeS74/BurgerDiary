48bbb595c37b76020f494b5f2d5029c7ac121527
Michael Sanchez <futurefreq@hotmail.com>
2017-11-30 00:25:21 -0800

    removed method override

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+//var methodOverride = require("method-override");
 
 var port = process.env.PORT || 3030;
 var app = express();
